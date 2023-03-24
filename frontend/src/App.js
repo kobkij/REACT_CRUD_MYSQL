992c3c3d76078e7e4bc47082d99181ec9477ea3f
kobkij <Kobkij@globofoods.com>
2023-03-24 13:56:33 +0700

    upload Project to git

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+//import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes,Switch } from 'react-router-dom'
+import ProductList from "./components/ProductList";
+import AddProduct from "./components/AddProduct";
+import EditProduct from "./components/EditProduct";
+ 
+function App() {
+  return (
+    <Router>
+    <div className="container">
+      <div className="columns">
+        <div className="column is-half is-offset-one-quarter">
+          <Switch>
+            <Route exact path="/">
+              <ProductList />
+            </Route>
+            <Route path="/add">
+              <AddProduct />
+            </Route>
+            <Route path="/edit/:id">
+              <EditProduct />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </div>
+    </Router>
+  );
+}
+ 
+export default App;
